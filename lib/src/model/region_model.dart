dc037bc7007c1f576415333c824018d6ad7be436
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-05-21 12:36:06 +0500

    Initial commit

---
+++ lib/src/model/region_model.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+List<RegionModel> regionModelFromJson(String str) => List<RegionModel>.from(
+    json.decode(str).map((x) => RegionModel.fromJson(x)));
+
+class RegionModel {
+  RegionModel({
+    required this.id,
+    required this.name,
+    required this.slug,
+    required this.childs,
+  });
+
+  int id;
+  String name;
+  String slug;
+  List<RegionModel> childs;
+
+  factory RegionModel.fromJson(Map<String, dynamic> json) => RegionModel(
+        id: json["id"] ?? 0,
+        name: json["name"] ?? "",
+        slug: json["slug"] ?? "",
+        childs: json["childs"] == null
+            ? <RegionModel>[]
+            : List<RegionModel>.from(
+                json["childs"].map((x) => RegionModel.fromJson(x))),
+      );
+}
---
+++ lib/src/ui/region_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:region6/src/model/region_model.dart';
+
+class RegionScreen extends StatefulWidget {
+  const RegionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegionScreen> createState() => _RegionScreenState();
+}
+
+class _RegionScreenState extends State<RegionScreen> {
+  List<RegionModel> data = [];
+
+  @override
+  void initState() {
+    _getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+              child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Text(data[index].name),
+                        ListView.builder(
+                          shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: data[index].childs.length,
+                            itemBuilder: (context, position) {
+                              return Text(
+                                  data[index].childs[position].id.toString());
+                            }
+                        )
+                      ],
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+
+  void _getData() async {
+    String url =
+        "https://api.osonapteka.uz/api/v1/regions?lan=uz&user=4ec8cc4ef07d43e6";
+    print(url);
+    http.Response response = await http.get(Uri.parse(url));
+    print(response.body);
+    data = regionModelFromJson(response.body);
+    setState(() {});
+  }
+}
